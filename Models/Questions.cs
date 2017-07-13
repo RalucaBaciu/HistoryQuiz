a65d5c211ef14a0f7cc6fa9c8d05ff43f0918c0f
RalucaBaciu <baciu.raluca.ionela@gmail.com>
2017-07-13 12:56:46 +0100

    Add files via upload

---
+++ Models/Questions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HistoryQuiz.Models
+{
+    public class Questions
+    {
+        public List<Question> questions { get; set;}
+    }
+}
